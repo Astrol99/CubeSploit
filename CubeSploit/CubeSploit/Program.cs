f69d8ec3c7a8527d89289ade8691cca3175cbba7
Astr0l <41875791+Astr0l@users.noreply.github.com>
2019-04-09 18:13:58 -0500

    Added memory modifiers

--- CubeSploit/CubeSploit/Program.cs
+++ CubeSploit/CubeSploit/Program.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace CubeSploit
 {
     class Program
     {
+        public static int BASE = 0;
+        public static int HEALTH = 0;
+
         static void Main(string[] args)
         {
+            VAMemory vam = new VAMemory("ac_client");
+            int LOCAL_PLAYER = vam.ReadInt32((IntPtr)BASE);
+
+            while (true)
+            {
+                int HEALTH_ADDRESS = LOCAL_PLAYER + HEALTH;
+                vam.WriteInt32((IntPtr)HEALTH_ADDRESS, 9999);
+
+                Thread.Sleep(200);
+            }
         }
     }
 }
